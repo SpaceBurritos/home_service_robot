785a754ad10dfe37fdfee8d1f21addad5ed11c6c
SpaceBurritos <59981149+SpaceBurritos@users.noreply.github.com>
2020-05-06 15:41:23 -0600

    Update test_navigation.sh

--- catkin_ws/src/scripts/test_navigation.sh
+++ catkin_ws/src/scripts/test_navigation.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export TURTLEBOT_GAZEBO_WORLD_FILE=/home/workspace/catkin_ws/src/worlds/myWorld.world
-export TURTLEBOT_GAZEBO_MAP_FILE=/home/workspace/catkin_ws/src/map/map.pgm
+export TURTLEBOT_GAZEBO_MAP_FILE=/home/workspace/catkin_ws/src/map/map.yaml
 export ROBOT_INITIAL_POSE="-x 22 -y 4 -Y 0"
 
 xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch " &

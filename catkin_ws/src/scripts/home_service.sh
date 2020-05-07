9217e878f167d825b1abce94517eec6babb04668
SpaceBurritos <59981149+SpaceBurritos@users.noreply.github.com>
2020-05-06 18:45:17 -0600

    Update home_service.sh

--- catkin_ws/src/scripts/home_service.sh
+++ catkin_ws/src/scripts/home_service.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-export TURTLEBOT_GAZEBO_WORLD_FILE=/home/workspace/catkin_ws/src/worlds/myWorld.world
-export TURTLEBOT_GAZEBO_MAP_FILE=/home/workspace/catkin_ws/src/map/map.yaml
 export ROBOT_INITIAL_POSE="-x 22 -y 4 -Y 0"
 
 xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find pick_objects)/worlds/myWorld.world" &

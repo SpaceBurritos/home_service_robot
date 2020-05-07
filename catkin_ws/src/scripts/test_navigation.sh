501e1a56eea7353e0661d5e25211a9245cb72f37
SpaceBurritos <59981149+SpaceBurritos@users.noreply.github.com>
2020-05-06 18:47:13 -0600

    Update test_navigation.sh

--- catkin_ws/src/scripts/test_navigation.sh
+++ catkin_ws/src/scripts/test_navigation.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-export TURTLEBOT_GAZEBO_WORLD_FILE=/home/workspace/catkin_ws/src/worlds/myWorld.world
-export TURTLEBOT_GAZEBO_MAP_FILE=/home/workspace/catkin_ws/src/map/map.yaml
 export ROBOT_INITIAL_POSE="-x 22 -y 4 -Y 0"
 
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch " &
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find pick_objects)/worlds/myWorld.world" &
 sleep 5
-xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch " &
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find pick_objects)/map/map.yaml" &
 sleep 5
 xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
